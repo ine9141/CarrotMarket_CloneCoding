7c47c3d41f04757359ca1b3482cb6b88f3781a79
ine <ine9141@gmail.com>
2022-11-30 09:27:45 +0900

    init setting (TEST)

--- app/src/main/java/com/example/myapplication/my.java
+++ app/src/main/java/com/example/myapplication/my.java
@@ -23,6 +23,51 @@ public class my extends AppCompatActivity {
         actionBar.hide();
 
         //하단 액션바
+        home = (Button) findViewById(R.id.home);
+        life = (Button) findViewById(R.id.life);
+        around = (Button) findViewById(R.id.around);
+        chat = (Button) findViewById(R.id.chat);
+        my = (Button) findViewById(R.id.my);
+
+        home.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(getApplicationContext(),home.class);
+                startActivity(intent);
+            }
+        });
+
+        life.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(getApplicationContext(),life.class);
+                startActivity(intent);
+            }
+        });
+
+        around.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(getApplicationContext(),around.class);
+                startActivity(intent);
+            }
+        });
+
+        chat.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(getApplicationContext(),chat.class);
+                startActivity(intent);
+            }
+        });
+
+        my.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(getApplicationContext(),my.class);
+                startActivity(intent);
+            }
+        });
 
 
     }
