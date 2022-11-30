b9103079d9f0865944982a0cc5c27822fb3488ac
ine <ine9141@gmail.com>
2022-11-30 11:36:24 +0900

    Login xml update

--- app/src/main/java/com/example/myapplication/MainActivity.java
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -11,63 +11,14 @@ import android.widget.ImageView;
 import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
-    Button home, life, around, chat, my;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
 
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        //상단 액션바 제거
-        ActionBar actionBar = getSupportActionBar();
-        actionBar.hide();
-
-        //하단 액션바
-        home = (Button) findViewById(R.id.home);
-        life = (Button) findViewById(R.id.life);
-        around = (Button) findViewById(R.id.around);
-        chat = (Button) findViewById(R.id.chat);
-        my = (Button) findViewById(R.id.my);
-
-        home.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Intent intent = new Intent(getApplicationContext(),home.class);
-                startActivity(intent);
-            }
-        });
-
-        life.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Intent intent = new Intent(getApplicationContext(),life.class);
-                startActivity(intent);
-            }
-        });
-
-        around.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Intent intent = new Intent(getApplicationContext(),around.class);
-                startActivity(intent);
-            }
-        });
-
-        chat.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Intent intent = new Intent(getApplicationContext(),chat.class);
-                startActivity(intent);
-            }
-        });
-
-        my.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Intent intent = new Intent(getApplicationContext(),my.class);
-                startActivity(intent);
-            }
-        });
+        Intent intent = new Intent(getApplicationContext(),login.class);
+        startActivity(intent);
 
     }
 }
---
+++ app/src/main/java/com/example/myapplication/login.java
@@ -0,0 +1,74 @@
+package com.example.myapplication;
+
+import androidx.appcompat.app.ActionBar;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.Toast;
+
+public class login extends AppCompatActivity {
+    Button home, life, around, chat, my;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.around);
+
+        //상단 액션바 제거
+        ActionBar actionBar = getSupportActionBar();
+        actionBar.hide();
+
+        //하단 액션바
+        home = (Button) findViewById(R.id.home);
+        life = (Button) findViewById(R.id.life);
+        around = (Button) findViewById(R.id.around);
+        chat = (Button) findViewById(R.id.chat);
+        my = (Button) findViewById(R.id.my);
+
+        home.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(getApplicationContext(),home.class);
+                startActivity(intent);
+            }
+        });
+
+        life.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(getApplicationContext(),life.class);
+                startActivity(intent);
+            }
+        });
+
+        around.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(getApplicationContext(),around.class);
+                startActivity(intent);
+            }
+        });
+
+        chat.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(getApplicationContext(),chat.class);
+                startActivity(intent);
+            }
+        });
+
+        my.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(getApplicationContext(),my.class);
+                startActivity(intent);
+            }
+        });
+
+
+    }
+}
