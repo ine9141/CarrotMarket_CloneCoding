c00d8e3ab5e645d4889ae715a3a8158df707988d
ine <ine9141@gmail.com>
2022-12-06 03:26:42 +0900

    지도만 추가 (채팅에 보내기 구상중)

--- app/src/main/java/com/example/myapplication/chat_room/location_share.java
+++ app/src/main/java/com/example/myapplication/chat_room/location_share.java
@@ -1,11 +1,21 @@
 package com.example.myapplication.chat_room;
 
+import android.Manifest;
 import android.app.FragmentManager;
+import android.content.Context;
+import android.content.pm.PackageManager;
+import android.location.Location;
+import android.location.LocationManager;
+import android.os.Build;
 import android.os.Bundle;
+import android.util.Log;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
 
 import com.example.myapplication.R;
+import com.example.myapplication.check_address;
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.MapFragment;
@@ -15,28 +25,48 @@ import com.google.android.gms.maps.model.MarkerOptions;
 
 public class location_share extends AppCompatActivity implements OnMapReadyCallback {
 
+    double longitude;
+    double latitude;
+    GoogleMap map;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.location_share);
 
-        FragmentManager fragmentManager = getFragmentManager();
-        MapFragment mapFragment = (MapFragment) fragmentManager.findFragmentById(R.id.map);
-        mapFragment.getMapAsync(this);
+        final LocationManager lm = (LocationManager) getSystemService(Context.LOCATION_SERVICE);
+
+        if (Build.VERSION.SDK_INT >= 23 &&
+                ContextCompat.checkSelfPermission(getApplicationContext(), Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+            ActivityCompat.requestPermissions(location_share.this, new String[]{
+                    Manifest.permission.ACCESS_FINE_LOCATION}, 0);
+        } else {
+            // 가장최근 위치정보 가져오기
+            Location location = lm.getLastKnownLocation(LocationManager.GPS_PROVIDER);
+
+            if (location != null) {
+                longitude = location.getLongitude();
+                latitude = location.getLatitude();
+
+                FragmentManager fragmentManager = getFragmentManager();
+                MapFragment mapFragment = (MapFragment) fragmentManager.findFragmentById(R.id.map);
+                mapFragment.getMapAsync(this);
 
 
+            }
+        }
     }
 
     @Override
     public void onMapReady(final GoogleMap map) {
 
-        LatLng SEOUL = new LatLng(37.56, 126.97);
+
+        LatLng SEOUL = new LatLng(latitude, longitude);
 
         MarkerOptions markerOptions = new MarkerOptions();
         markerOptions.position(SEOUL);
-        markerOptions.title("서울");
-        markerOptions.snippet("한국의 수도");
+        markerOptions.title("내 위치");
         map.addMarker(markerOptions);
 
         map.moveCamera(CameraUpdateFactory.newLatLng(SEOUL));
