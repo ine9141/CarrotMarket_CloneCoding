6d962e923fba58903f46f20b620d4dc601b1d21e
DH <donghyeon9511@gmail.com>
2022-12-10 18:53:04 +0900

    가게추천, 쿠폰리스트 구현

--- app/src/main/java/com/example/myapplication/around.java
+++ app/src/main/java/com/example/myapplication/around.java
@@ -2,25 +2,37 @@ package com.example.myapplication;
 
 import androidx.appcompat.app.ActionBar;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.content.ContextCompat;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
+import android.content.Context;
 import android.content.Intent;
+import android.graphics.drawable.Drawable;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.ImageView;
+import android.widget.ListView;
 import android.widget.TextView;
 import android.widget.Toast;
+
+import java.util.ArrayList;
 
 public class around extends AppCompatActivity {
     Button home, life, around, chat, my;
     TextView dong_name;
     String s,nick_name;
+    ListView rec_list;
+    RecyclerView coupon_list;
+    listitemadapter rec_listadapter;
+    cardviewadapter cou_adapter;
+    ArrayList<cardviewitem> c_list = new ArrayList<>();
     @Override
     protected void onCreate(Bundle savedInstanceState) {
 
         super.onCreate(savedInstanceState);
         setContentView(R.layout.around);
-
         //상단 액션바 제거
         ActionBar actionBar = getSupportActionBar();
         actionBar.hide();
@@ -32,6 +44,39 @@ public class around extends AppCompatActivity {
         chat = (Button) findViewById(R.id.chat);
         my = (Button) findViewById(R.id.my);
         dong_name = (TextView) findViewById(R.id.dong_name);
+
+        //게시글 리스트
+        rec_list = (ListView) findViewById(R.id.rec_list);
+        coupon_list = (RecyclerView) findViewById(R.id.coupon_list) ;
+
+
+        //추천가게 리스트
+        rec_listadapter = new listitemadapter();
+        rec_listadapter.additem(ContextCompat.getDrawable(
+                this,R.drawable.around), "A가게", "좋았어요", "하루 전");
+        rec_listadapter.additem(ContextCompat.getDrawable(
+                this,R.drawable.around), "B가게", "맛없었어요", "이틀 전");
+        rec_listadapter.additem(ContextCompat.getDrawable(
+                this,R.drawable.around), "C가게", "사장님이 불친절해요", "사흘 전");
+
+        rec_list.setAdapter(rec_listadapter);
+
+        addItem(ContextCompat.getDrawable(
+                this,R.drawable.chat),"A가게", "A요리를 하는 가게", "AAAAA");
+        addItem(ContextCompat.getDrawable(
+                this,R.drawable.chat),"A가게", "A요리를 하는 가게", "BBBB");
+        addItem(ContextCompat.getDrawable(
+                this,R.drawable.chat),"A가게", "A요리를 하는 가게", "CCCCC");
+        addItem(ContextCompat.getDrawable(
+                this,R.drawable.chat),"A가게", "A요리를 하는 가게", "DDDD");
+        addItem(ContextCompat.getDrawable(
+                this,R.drawable.chat),"A가게", "A요리를 하는 가게", "EEEEE");
+
+        cou_adapter = new cardviewadapter(c_list);
+        coupon_list.setAdapter(cou_adapter);
+        coupon_list.setLayoutManager(new LinearLayoutManager(this, RecyclerView.HORIZONTAL,false));
+        cou_adapter.notifyDataSetChanged();
+
 
         //동네이름 설정
         Intent secondIntent = getIntent();
@@ -94,5 +139,15 @@ public class around extends AppCompatActivity {
                 startActivity(intent);
             }
         });
+    }
+    public void addItem(Drawable img, String shop_name, String shop_content, String content) {
+        cardviewitem item = new cardviewitem();
+
+        item.setDrawable(img);
+        item.setShop_name(shop_name);
+        item.setShop_content(shop_content);
+        item.setContent(content);
+
+        c_list.add(item);
     }
 }
